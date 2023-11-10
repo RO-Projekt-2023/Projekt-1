2cd0219700408e8204e3c467f510500ee794fecf
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-11-10 18:44:45 +0100

    edited shit

--- projekt/resources/archive.blade.php
+++
@@ -1,4 +0,0 @@
-<form action="{{ route('search') }}" method="GET">
-    <input type="text" name="query" placeholder="Išči...">
-    <button type="submit">Iskanje</button>
-</form>
--- projekt/routes/web.php
+++ projekt/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\ArchiveController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/archive', 'ArchiveController@index')->name('archive');
+
+Route::get('/archive', function () {
+    return view('archive');
+});
+
 Route::get('/search', 'ArchiveController@search')->name('search');
 
