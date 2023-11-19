7eb17e03a24d9dfd6f9e527eb95d2e42ce59e4bc
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-11-19 22:02:02 +0100

    .

--- resources/views/archive.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('navbar')
-
-
-@section('content')
-    <br>
-    <br>
-    <h1>MUHAHAHAH</h1>
-@endsection
--- resources/views/events/archive.blade.php
+++ resources/views/events/archive.blade.php
@@ -1,14 +1,22 @@
 <!-- resources/views/events/archive.blade.php -->
 
-<h1>Archived Events</h1>
 
-@if(count($inactiveEvents) > 0)
-    @foreach($inactiveEvents as $event)
-        <h2>{{ $event->name }}</h2>
-        <p>Description: {{ $event->description }}</p>
-        <p>Date: {{ $event->date }}</p>
-        <p>Price of Tickets: {{ $event->price_of_tickets }}</p>
-    @endforeach
-@else
-    <p>No archived events available.</p>
-@endif
+@extends('navbar')
+
+
+@section('content')
+    <div class="events-container" style="margin-top: 60px;">
+    <h1>Archived Events</h1>
+
+    @if(count($inactiveEvents) > 0)
+        @foreach($inactiveEvents as $event)
+            <h2>{{ $event->name }}</h2>
+            <p>Description: {{ $event->description }}</p>
+            <p>Date: {{ $event->date }}</p>
+            <p>Price of Tickets: {{ $event->price_of_tickets }}</p>
+        @endforeach
+    @else
+        <p>No archived events available.</p>
+    @endif
+    </div>
+@endsection
