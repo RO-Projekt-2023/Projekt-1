fd6fe58bb129e65d0389a0f3d165aed0c2a3f996
suddendeath91 <144319788+suddendeath91@users.noreply.github.com>
2023-11-29 00:09:43 +0100

    events css

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -5,19 +5,30 @@
 @section('content')
 <div class="events-container" style="margin-top: 60px;">
     <h2>{{ $event->name }}</h2>
-    <p>Description: {{ $event->description }}</p>
-    <p>Date: {{ $event->date }}</p>
-    <p>Price of Tickets: {{ $event->price_of_tickets }}</p>
-    <p>Number of Tickets: {{ $event->number_of_tickets }}</p>
 
+    <div id="info">
+        <h4>Date:</h4>
+        <p>{{ $event->date }}</p>
+        <h4>Price of Tickets:</h4>
+        <p>{{ $event->price_of_tickets }}€</p>
+        <h4>Number of Tickets:</h4>
+        <p>{{ $event->number_of_tickets }}</p>
+    </div>
+    <div id="desc">
+        <h4>Description:</h4>
+        <p>{{ $event->description }}</p>
+    </div>
     <!-- Display other event details as needed -->
 
     <!-- Ticket Application Form -->
-    <form action="{{ route('events.apply', $event->id) }}" method="POST">
-        @csrf
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        <button type="submit">Apply for Ticket</button>
-    </form>
+    <div id="f">
+        <form action="{{ route('events.apply', $event->id) }}" method="POST">
+            @csrf
+            <h4><label for="email">Email:</label></h4>
+            <p>Če želite dobiti vstopnico za dogodek, spodaj vpišite vaš e-poštni naslov!</p>
+            <input type="email" id="email" name="email" required>
+            <button type="submit">Apply for Ticket</button>
+        </form>
+    </div>
 </div>
 @endsection
